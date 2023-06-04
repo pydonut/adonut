fc38fbfbee43ad5eba47c94a7522bb620fafd6f9
dan-2ee <ekdms7273@naver.com>
2023-06-04 23:46:35 +0900

    질문 수정 횟수 view

--- pybo/models.py
+++ pybo/models.py
@@ -9,6 +9,7 @@ class Question(models.Model):
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_question')
+    modify_count = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.subject
